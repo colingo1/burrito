aa65c65275228d76e302ee6790a4715ba2a7a35c
warde <warde@rpi.edu>
2017-11-11 14:22:08 -0500

    Initial Commit

---
+++ app/src/main/java/com/crowdsourced_burrito/burrito/MainActivity.java
@@ -0,0 +1,19 @@
+package com.crowdsourced_burrito.burrito;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText("(Skree-EEEE)");
+    }
+
+}
